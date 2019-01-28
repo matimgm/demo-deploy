c2413a9349b4a6fd1944f4f44beb69d12fe3ca76
Matias Martini <matias.martini@outlook.com>
2019-01-28 14:39:07 -0300

    Arrgloe --aot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  { path: '**', component: AppComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,  {useHash: true})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
